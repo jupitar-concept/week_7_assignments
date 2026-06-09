272987e7abcdc4079877543c21ed74129f33f346
Jupitar Concept <asiaadex@gmail.com>
2026-06-09 17:07:52 +0100

    Completed Question 1

---
+++ question1.js
@@ -0,0 +1,41 @@
+// Question 1:Create Student Objects
+const students = [
+  {
+    id: 1,
+    name: "Seyi Makinde",
+    age: 20,
+    grades: [85, 92, 78],
+  },
+  {
+    id: 2,
+    name: "Babajide Sanwo-Olu",
+    age: 21,
+    grades: [88, 76, 90],
+  },
+  {
+    id: 3,
+    name: "Ademola Adeleke",
+    age: 19,
+    grades: [95, 89, 93],
+  },
+  {
+    id: 4,
+    name: "Dapo Abiodun,
+    age: 22,
+    grades: [70, 82, 75],
+  },
+  {
+    id: 5,
+    name: "Biodun Oyebanji",
+    age: 20,
+    grades: [91, 87, 84],
+  },
+  {
+    id: 6,
+    name: "Lucky Aiyedatiwa",
+    age: 21,
+    grades: [78, 85, 80],
+  }
+];
+
+console.log(students);
